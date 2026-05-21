e56343a3055a3b96f6540ab4b934591df9fa80bd
huzaifa-dpt <mhuzaifa.bscs20seecs@seecs.edu.pk>
2026-05-21 13:05:56 +0300

    -

--- habit-tracker/src/models/Habit.jsx
+++
@@ -1,9 +0,0 @@
-class Habit {
-  constructor(habitName = "") {
-    this.unique_key = crypto.randomUUID();
-    this.habit = habitName.trim();
-    this.completions = [];
-  }
-}
-
-export default Habit;
